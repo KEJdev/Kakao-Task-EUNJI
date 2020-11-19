7c1b003a8bf4459b844cefb42ede298151e03699
KEJdev <kej12516@gmail.com>
2020-11-19 23:34:48 +0900

    SQL 수정

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # 과제 
 
-------------
-
 ### Task 2
 
 - 도커 이미지 오라클11g 사용. 
 - SQL Developer 툴 사용.
 
-조건에 맞게 테이블을 2개의 view로 생성 후 차집합 연산자를 사용하였다.  
+조건에 맞는 테이블을 view로 생성 후 합집합 연산자를 사용하였다.  
 
 **create view**  
 1. payment : 2019년 12월에 1만원 이상 결제한 유저들
 2. cancel_user : 12월에 결제를 했지만 12월에서 2월 사이에 취소한 사람들
+3. dutchpay_claim : 더치페이를 요청한 유저
+4. payment_user : 취소 반영한 순결제금액 1만원 이상인 유저들
+
 
 **output**  
-payment - cancel_user (차집합 연산)
+payment_user ∩ dutchpay (교집합)
+
--- Task2/Task2.sql
+++ Task2/Task2.sql
@@ -1,20 +1,28 @@
 SELECT * from a_payment;
+SELECT * from dutchpay;
 
--- 1. 2019년 12월에 1만원 이상 결제한 유저들
+-- 1. 더치페이를 요청한 유저
+create view dutchpay_claim
+as
+SELECT claim_user_id
+from dutchpay;
+where claim_at between to_date('19/12/01', 'RR/MM/DD') and to_date('19/12/31', 'RR/MM/DD');
+
+--2. a 가맹점에서 2019년 12월에 1만원 이상 결제한 유저들
 create view payment
 as
 SELECT user_id, id, transacted_at, amount, payment_action_type
 from a_payment
 where transacted_at between to_date('19/12/01', 'RR/MM/DD') and to_date('19/12/31', 'RR/MM/DD') and amount>=10000 ;
 
---2. 12월에 결제를 했지만 12월에서 2월 사이에 취소한 사람들
+--3. 12월에서 2월 사이에 결제를 취소한 사람들
 create view cancel_user
 as
 select user_id,id,  amount, payment_action_type
 from a_payment
 where transacted_at between to_date('19/12/01', 'RR/MM/DD') and to_date('20/02/29', 'RR/MM/DD') and payment_action_type = 'CANCEL';
 
---3. 취소를 반영한 순결제금액 1만원 이상인 유저들
+--3. 취소 반영한 순결제금액 1만원 이상인 유저들
 create view payment_user
 as
 select user_id, id, transacted_at, amount, payment_action_type
@@ -24,9 +32,6 @@ minus
 select id from cancel_user);
 
 --4. 더치페이를 요청한 유저 중 a가맹점에서 2019년 12월에 1만원 이상 결제한 유저
-SELECT user_id from payment_user;
-
--- user id 중복을 해야한다면
-SELECT distinct user_id from payment_user;
-
-
+SELECT user_id from payment_user
+intersect
+SELECT claim_user_id from dutchpay;
